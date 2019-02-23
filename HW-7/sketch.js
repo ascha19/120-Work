1eab7021acb567aaec295b8e8e67524e2fed0144
Avery <46611206+aschave19@users.noreply.github.com>
2019-02-22 21:21:35 -0700

    Sketch alterations

--- HW-7/sketch.js
+++ HW-7/sketch.js
@@ -1,9 +1,9 @@
 
 
 let ball = {};
-ball.width = 40;
-ball.x = 10;
-ball.y = 10;
+ball.width = 50;
+ball.x = 15;
+ball.y = 15;
 ball.delta_x = 1;
 ball.delta_y = 1;
 ball.scale_x = 1;
@@ -11,29 +11,29 @@ ball.scale_y = 1;
 
 function setup() {
     createCanvas(windowWidth, 400);
-    background(255);
+    background(247);
 }
 
 
 
 function draw() {
 
-    ball.x += ball.delta_x * ball.scale_x;
-    ball.y += ball.delta_y * ball.scale_y;
+    ball.x += ball.delta_x * ball.scale_y;
+    ball.y += ball.delta_y * ball.scale_x;
 
 
     if (ball.x >= width || ball.x <= 0) {
-        ball.delta_x = -1 * ball.delta_x;
+        ball.delta_x = 1 * -ball.delta_x;
     }
     if (ball.y >= height || ball.y <= 0) {
-        ball.delta_y = -1 * ball.delta_y;
+        ball.delta_y = 1 * -ball.delta_y;
     }
 
-    fill(255);
+    fill(0);
     ellipse(ball.x, ball.y, ball.width, ball.width);
 }
 
 function mousePressed() {
-    ball.scale_x = map(mouseX, 0, width, 0.5, 10);
-    ball.scale_y = map(mouseY, 0, height, 0.5, 10);
+    ball.scale_x = map(mouseY, 0, width, 0, 10);
+    ball.scale_y = map(mouseX, 0, height, 0, 10);
 }
