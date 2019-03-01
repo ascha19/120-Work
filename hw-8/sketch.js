d7b3746dacba8dcc728f5a42d8a2239d539dfd71
Avery <46611206+aschave19@users.noreply.github.com>
2019-03-01 15:02:14 -0700

    hw-8; circle-y background

---
+++ hw-8/sketch.js
@@ -0,0 +1,28 @@
+//I wanted to make something like one of those flashing arrow construction signs over a disco-feeling background by altering two of the codes provided with this week's reading. Let's see how this goes.
+//Warning: Flashing colors in sketch
+
+function setup(){
+
+  //Creating canvas and altering the frame rate so the flashing colors aren't so aggressively flashing
+  createCanvas(windowWidth, 300);
+  frameRate(15);
+}
+
+function draw(){
+
+//Black background to balance out the colors that will be added later
+//Removed stroke on circles because I think it looks softer and cooler
+  background(0);
+  noStroke();
+
+//First for loop in the nested loop
+  for (let y = 0; y <= height; y += 8) {
+//Nested for loop
+    for (let x = 0; x <= width; x += 10) {
+//Colors randomized for pizazze. Can't decide between colors for black and white, so I guess we'll see what I decide when I post.
+      fill(random(255));
+//Drawing the actual circle
+      ellipse(x + y, y, 16 - y/10, 16 - y/10);
+    }
+  }
+}
