3b4688919c0c9971440d8b32974bda3dffce2294
Avery <46611206+aschave19@users.noreply.github.com>
2019-03-17 11:10:52 -0600

    hw-10 attempts

---
+++ hw-10/sketch.js
@@ -0,0 +1,26 @@
+//I felt like making a dumb joke and wanted to see if I could make images for it. If anyone gets it, bless.
+
+//Array and functions time
+let dumbArray = ["Bone", "Apple", "Teeth"];
+
+let background_color = (196, 217, 224);
+
+function bone() {
+
+}
+
+//Basic setup stuff; canvas, framerate, background color, etc
+function setup() {
+  createCanvas(800, 400);
+  frameRate(4);
+
+  let index = 0;
+}
+
+//Basic setup stuff; background, text size, etc
+function draw();
+  background(background_color);
+  textSize(27);
+
+  //Writing the text
+  text("When the meal's real good:" + index, 200, 200);
