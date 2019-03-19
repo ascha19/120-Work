ce25e025eb9832c0681520674c1211a4883b545b
Avery <46611206+aschave19@users.noreply.github.com>
2019-03-18 22:15:37 -0600

    hw-10

--- hw-10/sketch.js
+++ hw-10/sketch.js
@@ -3,27 +3,71 @@
 //Array and functions time
 let dumbArray = ["Bone", "Apple", "Teeth"];
 
-let background_color = ('rgb(201, 219, 227)');
+let bgColor = 200;
 
-function bone(); {
-
-}
+let idx = 0;
 
 //Basic setup stuff; canvas, framerate, background color, etc
 function setup(); {
   createCanvas(800, 400);
-  frameRate(4);
-
-  let idx = 0;
+  frameRate(5);
 }
 
 //Basic setup stuff; background, text size, etc
 function draw(); {
-  background(background_color);
-  textSize(27);
+  background(bgColor);
+  textSize(20);
 
-  //Writing the text
+//Writing the text
   text("When the meal's real good:" + dumbArray[idx], 200, 200);
-
+//Making sure array boundaries are followed
   idx = (idx + 1) % dumbArray.length;
+
+//Allowing new functions to be called
+  bone();
+  apple();
+  teeth();
 }
+
+//Drawing the bone
+function bone(); {
+
+  stroke(255);
+  fill('rgb(254, 255, 213)');
+  rect(0, 0, 20, 10);
+  circle(0, 0, 2.5);
+  circle(0, 10, 2.5);
+  circle(20, 0, 2.5);
+  circle(20, 10, 2.5);
+
+}
+
+//Drawing the apple
+function apple(); {
+
+//Body
+  noStroke();
+  fill('rgb(175, 0, 5)');
+  circle(0, 0, 10);
+
+//Stem
+  fill('black');
+  line(0, 0, 20, 20);
+
+}
+
+//Drawing the teeth
+function teeth(); {
+
+//Mouth
+  stroke('black');
+  arc(0, 0, 20, 10, 0, PI + QUARTER_PI, CHORD);
+
+//Teeth line/Gap tooth
+  strokeWeight(5);
+  line(0, 0, 0, 20);
+
+}
+
+//Attempt to make pictures move with mouse
+if 
