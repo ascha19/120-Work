c65ee87d55a1642174b1d26752d2cad02e703d85
Avery <46611206+aschave19@users.noreply.github.com>
2019-03-19 21:58:47 -0600

    hw-10

--- hw-10/sketch.js
+++ hw-10/sketch.js
@@ -7,67 +7,96 @@ let bgColor = 200;
 
 let idx = 0;
 
+let arrayWord = dumbArray[idx];
+let boneBoi = 'function bone();'
+let appleLady = 'function apple();'
+let smileyGap = 'function teeth();'
+
 //Basic setup stuff; canvas, framerate, background color, etc
-function setup(); {
+function setup() {
   createCanvas(800, 400);
-  frameRate(5);
+  frameRate(1);
+
 }
 
 //Basic setup stuff; background, text size, etc
-function draw(); {
+function draw() {
   background(bgColor);
   textSize(20);
+  noStroke();
+  fill('black');
 
 //Writing the text
-  text("When the meal's real good:" + dumbArray[idx], 200, 200);
+  text("When the meal's real good: " + dumbArray[idx], 200, 200);
 //Making sure array boundaries are followed
-  idx = (idx + 1) % dumbArray.length;
+  idx = (idx+1) % 3;
 
 //Allowing new functions to be called
-  bone();
-  apple();
-  teeth();
+    bone();
+    apple();
+    teeth();
+
 }
 
 //Drawing the bone
-function bone(); {
+function bone(){
 
-  stroke(255);
-  fill('rgb(254, 255, 213)');
-  rect(0, 0, 20, 10);
-  circle(0, 0, 2.5);
-  circle(0, 10, 2.5);
-  circle(20, 0, 2.5);
-  circle(20, 10, 2.5);
+  if (arrayWord === dumbArray[0]) {
 
+    //stroke(255);
+    //strokeWeight(2);
+    noStroke();
+    fill(254, 255, 213);
+    rect(50, 50, 40, 20);
+
+    ellipse(50, 50, 20);
+    ellipse(90, 50, 20);
+    ellipse(90, 70, 20);
+    ellipse(50, 70, 20);
+  }
+else {}
 }
 
+
 //Drawing the apple
-function apple(); {
+function apple(){
 
-//Body
-  noStroke();
-  fill('rgb(175, 0, 5)');
-  circle(0, 0, 10);
+if (arrayWord === dumbArray[1]) {
 
-//Stem
-  fill('black');
-  line(0, 0, 20, 20);
+  //Stem
+      stroke(2);
+      fill('brown');
+      line(100, 100, 100, 70);
+
+  //Body
+    noStroke();
+    fill(175, 0, 5);
+    ellipse(100,100, 40);
+   }
+else{}
 
 }
 
 //Drawing the teeth
-function teeth(); {
+function teeth(){
 
-//Mouth
-  stroke('black');
-  arc(0, 0, 20, 10, 0, PI + QUARTER_PI, CHORD);
+if (arrayWord === dumbArray[2]) {
 
-//Teeth line/Gap tooth
-  strokeWeight(5);
-  line(0, 0, 0, 20);
+  //Mouth
+    stroke('black');
+    strokeWeight(2);
+    fill('white');
+    arc(100, 200, 40, 40, 0, PI + QUARTER_PI, CHORD);
+
+  //Teeth line/Gap tooth
+    strokeWeight(5);
+    line(100, 193, 90, 215);
+   }
+
+else{}
 
 }
 
-//Attempt to make pictures move with mouse
-if 
+//function mousePressed(); {
+
+//}
